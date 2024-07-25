ed21d9849815c9086c97defd00ff2dfa0e21a769
Dikshant <dikkpsd@gmail.com>
2024-07-25 20:23:36 +0530

    Added more layers, need to test

--- algorithms/nn/layers.py
+++ algorithms/nn/layers.py
@@ -51,7 +51,95 @@ class Dense:
         self.input = None
         self.output = None
     
+class Relu:
+    def __init__(self):
+        self.input = None
+        self.output = None
+        self.activation = relu
+        self.activation_derivative = relu_derivative
+    
+    def forward(self, input):
+        self.input = input
+        self.output = self.activation(input)
+        return self.output
+    
+    def backward(self, output_grad, lr):
+        return output_grad * self.activation_derivative(self.input)
+    
+    def __repr__(self):
+        return 'Relu'
+    
+    def __str__(self):
+        return 'Relu'
+    
+    def reset(self):
+        self.input = None
+        self.output = None
         
+class Sigmoid:
+    def __init__(self):
+        self.input = None
+        self.output = None
+        self.activation = sigmoid
+        self.activation_derivative = sigmoid_derivative
+    
+    def forward(self, input):
+        self.input = input
+        self.output = self.activation(input)
+        return self.output
+    
+    def backward(self, output_grad, lr):
+        return output_grad * self.activation_derivative(self.input)
+    
+    def __repr__(self):
+        return 'Sigmoid'
+    
+    def __str__(self):
+        return 'Sigmoid'
+    
+    def reset(self):
+        self.input = None
+        self.output = None
         
+class Flatten:
+    def __init__(self):
+        self.input = None
+        self.output = None
+    
+    def forward(self, input):
+        self.input = input
+        self.output = input.reshape(input.shape[0], -1)
+        return self.output
+    
+    def backward(self, output_grad, lr):
+        return output_grad.reshape(self.input.shape)
+    
+    def __repr__(self):
+        return 'Flatten'
+    
+    def __str__(self):
+        return 'Flatten'
+    
+    def reset(self):
+        self.input = None
+        self.output = None
+        
+
+# TODO: Implement Conv2D layer
+class Conv2D:
+    def __init__(self,n_channels, num_filter, kernel_size = (3,3), stride = 1, padding = 0):
+        
+        # output_shape = (*np.floor( (input_shape - kernel_size + 2 * padding) / stride + 1), num_filter)
+        self.num_filter = num_filter
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.weights = np.random.randn(num_filter, n_channels,*kernel_size)
+        self.biases = np.random.randn(num_filter)
+        self.input = None
+        self.output = None
+        self.input_padded = None
+
+    
         
         
