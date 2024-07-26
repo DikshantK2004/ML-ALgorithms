03fff46e5ab4e2a38b7a4196a685c2cc3645aea2
Dikshant <dikkpsd@gmail.com>
2024-07-26 15:42:44 +0530

    Added MaxPool and Conv2D

--- algorithms/nn/layers.py
+++ algorithms/nn/layers.py
@@ -1,5 +1,6 @@
 import numpy as np
 from .activations import sigmoid, relu, sigmoid_derivative, relu_derivative
+from scipy import signal, fft
 class Dense:
     def __init__(self, input_size, output_size, activation = None):
         self.input_size =  input_size
@@ -125,21 +126,162 @@ class Flatten:
         self.output = None
         
 
-# TODO: Implement Conv2D layer
+import numpy as np
+
 class Conv2D:
-    def __init__(self,n_channels, num_filter, kernel_size = (3,3), stride = 1, padding = 0):
-        
-        # output_shape = (*np.floor( (input_shape - kernel_size + 2 * padding) / stride + 1), num_filter)
+    def __init__(self, n_channels, num_filter, kernel_size=(3, 3), stride=1, padding=0):
+        self.n_channels = n_channels
         self.num_filter = num_filter
         self.kernel_size = kernel_size
         self.stride = stride
         self.padding = padding
-        self.weights = np.random.randn(num_filter, n_channels,*kernel_size)
+        self.weights = np.random.randn(num_filter, n_channels, *kernel_size)
         self.biases = np.random.randn(num_filter)
         self.input = None
         self.output = None
         self.input_padded = None
 
-    
+    def pad_input(self, input):
+        channels, row, cols = input.shape
+        input_padded = np.zeros((channels, row + 2*self.padding, cols + 2*self.padding))
+        input_padded[:, self.padding:row + self.padding, self.padding:cols + self.padding] = input
+        return input_padded
+
+    def compute_output_shape(self, input_shape):
+        channels, row, cols = input_shape
+        out_height = int((row + 2*self.padding - self.kernel_size[0]) / self.stride + 1)
+        out_width = int((cols + 2*self.padding - self.kernel_size[1]) / self.stride + 1)
+        return self.num_filter, out_height, out_width
+
+    def forward(self, input):
+        self.input = input
+        input_padded = self.pad_input(input)
+        self.input_padded = input_padded
+        channels, row, cols = input.shape
+        output_shape = self.compute_output_shape(input.shape)
+        self.output = np.zeros(output_shape)
+
+        for i in range(0, row + 2*self.padding - self.kernel_size[0] + 1, self.stride):
+            for j in range(0, cols + 2*self.padding - self.kernel_size[1] + 1, self.stride):
+                i_end = i + self.kernel_size[0]
+                j_end = j + self.kernel_size[1]
+                input_slice = input_padded[:, i:i_end, j:j_end]
+                self.output[:, int(i/self.stride), int(j/self.stride)] = (
+                    np.tensordot(input_slice, self.weights, axes=((0, 1, 2), (1, 2, 3))) + self.biases
+                )
+
+        return self.output
+
+    def backward(self, output_grad, lr):
+        _, out_height, out_width = output_grad.shape
+        grad_w = np.zeros_like(self.weights)
+        grad_b = np.zeros_like(self.biases)
+        grad_input = np.zeros_like(self.input_padded)
+
+        for i in range(out_height):
+            for j in range(out_width):
+                h_start = i * self.stride
+                h_end = h_start + self.kernel_size[0]
+                w_start = j * self.stride
+                w_end = w_start + self.kernel_size[1]
+
+                input_slice = self.input_padded[:, h_start:h_end, w_start:w_end]
+                for f in range(self.num_filter):
+                    grad_input[:, h_start:h_end, w_start:w_end] += (
+                        self.weights[f] * output_grad[f, i, j]
+                    )
+                    grad_w[f] += input_slice * output_grad[f, i, j]
+                    grad_b[f] += output_grad[f, i, j]
+
+        if self.padding != 0:
+            grad_input = grad_input[:, self.padding:-self.padding, self.padding:-self.padding]
+
+        self.weights -= lr * grad_w
+        self.biases -= lr * grad_b
+
+        return grad_input
+
+    def reset(self):
+        self.weights = np.random.randn(self.num_filter, self.n_channels, *self.kernel_size)
+        self.biases = np.random.randn(self.num_filter)
+        self.input = None
+        self.output = None
+        self.input_padded = None
+
+    def __repr__(self):
+        return f'Conv2D(num_filter={self.num_filter}, kernel_size={self.kernel_size}, stride={self.stride}, padding={self.padding})'
+
+    def __str__(self):
+        return f'Conv2D(num_filter={self.num_filter}, kernel_size={self.kernel_size}, stride={self.stride}, padding={self.padding})'
         
         
+        
+class MaxPool:
+    def __init__(self, pool_size=(2, 2), stride=1, padding=0):
+        self.pool_size = pool_size
+        self.stride = stride
+        self.padding = padding
+        self.input = None
+        self.output = None
+        self.input_padded = None
+
+    def pad_input(self, input):
+        channels, row, cols = input.shape
+        input_padded = np.zeros((channels, row + 2*self.padding, cols + 2*self.padding))
+        input_padded[:, self.padding:row + self.padding, self.padding:cols + self.padding] = input
+        return input_padded
+
+    def compute_output_shape(self, input_shape):
+        channels, row, cols = input_shape
+        out_height = int((row + 2*self.padding - self.pool_size[0]) / self.stride + 1)
+        out_width = int((cols + 2*self.padding - self.pool_size[1]) / self.stride + 1)
+        return channels, out_height, out_width
+
+    def forward(self, input):
+        self.input = input
+        input_padded = self.pad_input(input)
+        self.input_padded = input_padded
+        channels, row, cols = input.shape
+        output_shape = self.compute_output_shape(input.shape)
+        self.output = np.zeros(output_shape)
+
+        for i in range(0, row + 2*self.padding - self.pool_size[0] + 1, self.stride):
+            for j in range(0, cols + 2*self.padding - self.pool_size[1] + 1, self.stride):
+                i_end = i + self.pool_size[0]
+                j_end = j + self.pool_size[1]
+                input_slice = input_padded[:, i:i_end, j:j_end]
+                self.output[:, int(i/self.stride), int(j/self.stride)] = np.max(input_slice, axis=(1, 2))
+
+        return self.output
+
+    def backward(self, output_grad, lr=None):
+        _, out_height, out_width = output_grad.shape
+        grad_input = np.zeros_like(self.input_padded)
+
+        for i in range(out_height):
+            for j in range(out_width):
+                h_start = i * self.stride
+                h_end = h_start + self.pool_size[0]
+                w_start = j * self.stride
+                w_end = w_start + self.pool_size[1]
+
+                input_slice = self.input_padded[:, h_start:h_end, w_start:w_end]
+                max_vals = np.max(input_slice, axis=(1, 2), keepdims=True)
+                mask = (input_slice == max_vals)
+                grad_input[:, h_start:h_end, w_start:w_end] += mask * output_grad[:, i, j].reshape(-1, 1, 1)
+
+        if self.padding != 0:
+            grad_input = grad_input[:, self.padding:-self.padding, self.padding:-self.padding]
+
+        return grad_input
+
+    def reset(self):
+        self.input = None
+        self.output = None
+        self.input_padded = None
+
+    def __repr__(self):
+        return f'MaxPool(pool_size={self.pool_size}, stride={self.stride}, padding={self.padding})'
+
+    def __str__(self):
+        return f'MaxPool(pool_size={self.pool_size}, stride={self.stride}, padding={self.padding})'
